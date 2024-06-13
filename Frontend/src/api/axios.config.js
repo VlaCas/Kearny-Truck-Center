4312de0da91abaf4cf4fd1e9ae9be905fdb4db58
VlaCas <34453102+VlaCas@users.noreply.github.com>
2024-06-13 16:51:55 -0400

    updated axios.config

--- Frontend/src/api/axios.config.js
+++ Frontend/src/api/axios.config.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:7117/api',
+  baseURL: 'https://kearny-truck-center.onrender.com/api',
   withCredentials: true
 });
 
