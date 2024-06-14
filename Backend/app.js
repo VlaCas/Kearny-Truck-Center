18cf95714f878b0dfe4d87bf4bbb7c584ab13b89
VlaCas <34453102+VlaCas@users.noreply.github.com>
2024-06-14 16:02:55 -0400

    update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -5,7 +5,7 @@ import Routes from './router/sendEmail.routes.js';
 
 const app = express();
 // 'http://localhost:5000', 
-app.use(cors({ origin: 'https://kearnytruckcenter.com', credentials: true }));
+app.use(cors({ origin: ['https://www.kearnytruckcenter.com', 'https://kearnytruckcenter.com'], credentials: true }));
 app.use(morgan('dev'));
 app.use(express.json());  
 app.use('/api', Routes);
