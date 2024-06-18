49450fd5616ec3195001f3ee8fb33ac6b14825ec
VlaCas <34453102+VlaCas@users.noreply.github.com>
2024-06-18 13:56:12 -0400

    update
    add Google ads
    add Robots.txt to backend
    update ContactForm.jsx
    update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
+import path from 'path';
 import Routes from './router/sendEmail.routes.js';
 
 const app = express();
-// 'http://localhost:5000', 
 app.use(cors({ origin: ['https://www.kearnytruckcenter.com', 'https://kearnytruckcenter.com'], credentials: true }));
 app.use(morgan('dev'));
-app.use(express.json());  
+app.use(express.json());
 app.use('/api', Routes);
+app.use('/robots.txt', express.static(path.join(path.resolve(), 'robots.txt')));
 
 export default app;
